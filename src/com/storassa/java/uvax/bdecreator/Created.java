2f1d7f507f35189c9b88b6fb3bdde07df48fc13e
sergio.torassa <sthorass@gmail.com>
2017-10-11 11:43:50 +0200

    working

---
+++ src/com/storassa/java/uvax/bdecreator/Created.java
@@ -0,0 +1,133 @@
+package com.storassa.java.uvax.bdecreator;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+
+public class Created extends JDialog {
+
+	String name = "UP_NAPOLIL_4", startDate = "16-09-2011 10:15:00 L", endDate = "16-09-2011 10:28:00 L",
+			note = "Messaggio START", reason = "-77.812", creationDate = "16-09-2011 09:15:00 L";
+
+	private final JPanel contentPanel = new JPanel();
+	JTextArea BdeMessagetextArea;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			Created dialog = new Created();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Created() {
+		setResizable(false);
+		setBounds(100, 100, 696, 446);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			BdeMessagetextArea = new JTextArea();
+			BdeMessagetextArea.setEditable(false);
+			BdeMessagetextArea.setFont(new Font("Courier New", Font.PLAIN, 14));
+			contentPanel.add(BdeMessagetextArea);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Save");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// Create a file chooser
+						// final JFileChooser fc = new JFileChooser();
+						// int returnVal = fc.showOpenDialog(Created.this);
+						// if (returnVal == JFileChooser.APPROVE_OPTION) {
+						// File file = fc.getSelectedFile();
+						//
+						
+						File file = new File("sequenceNumber.txt");
+						
+						try {
+							PrintWriter bw = new PrintWriter(file);
+							bw.print(BdeMessagetextArea.getText());
+							bw.close();
+						} catch (Exception ex) {
+						}
+						// }
+						System.exit(0);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+	public Created(String _name, String _startDate, String _endDate, String _note, String _reason,
+			String _creationDate) {
+
+		this();
+
+		name = _name;
+		startDate = _startDate;
+		endDate = _endDate;
+		note = _note;
+		reason = _reason;
+		creationDate = _creationDate;
+
+		BdeMessagetextArea.setText("*************************************************\r\n"
+				+ "************** MESSAGGIO DI COMANDO *************\r\n"
+				+ "****************** PER UPA/UCA ******************\r\n"
+				+ "*************************************************\r\n" + "Nome UPA/UCA                        = "
+				+ name + "\r\n" + "Data Ora Inizio                     = " + startDate + "\r\n"
+				+ "Data Ora Fine                       = " + endDate + "\r\n" + "Motivazione                         = "
+				+ reason + "\r\n" + "Note                                = " + note + "\r\n"
+				+ "Data Creazione Msg                  = " + creationDate + "\r\n"
+				+ "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\r\n"
+				+ "++++++++++++++++++++++++++++++++++++++++++++++++++++++\r\n" + name + ";" + startDate + ";" + endDate
+				+ "\n" + reason + "\r\n"
+				+ "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\r\n"
+				+ "++++++++++++++++++++++++++++++++++++++++++++++++++++++\r\n"
+
+		);
+
+		this.setVisible(true);
+
+	}
+
+}
