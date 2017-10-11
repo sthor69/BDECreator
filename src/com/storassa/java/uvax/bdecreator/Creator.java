06d7c21ff63982aa882aa2c27df9cb40aad137aa
sergio.torassa <sthorass@gmail.com>
2017-10-11 11:45:34 +0200

    working

--- src/com/storassa/java/uvax/bdecreator/Creator.java
+++ src/com/storassa/java/uvax/bdecreator/Creator.java
@@ -7,16 +7,18 @@ import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Creator {
 
 	private JFrame frame;
-	private JTextField txtUpnapolil;
-	private JTextField txtL;
-	private JTextField txtL_1;
-	private JTextField txtMessaggioStart;
-	private JTextField textField_4;
-	private JTextField txtL_2;
+	private JTextField nameText;
+	private JTextField startDateText;
+	private JTextField endDateText;
+	private JTextField reasonText;
+	private JTextField noteText;
+	private JTextField creationDateText;
 
 	/**
 	 * Launch the application.
@@ -51,72 +53,87 @@ public class Creator {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Nome UPA/UCA");
-		lblNewLabel.setBounds(10, 11, 169, 14);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel nameLabel = new JLabel("Nome UPA/UCA");
+		nameLabel.setBounds(10, 11, 169, 14);
+		frame.getContentPane().add(nameLabel);
 		
-		JLabel lblDataOraInizo = new JLabel("Data Ora Inizo");
-		lblDataOraInizo.setBounds(10, 36, 169, 14);
-		frame.getContentPane().add(lblDataOraInizo);
+		JLabel startDateLabel = new JLabel("Data Ora Inizo");
+		startDateLabel.setBounds(10, 36, 169, 14);
+		frame.getContentPane().add(startDateLabel);
 		
-		JLabel lblDataOraFine = new JLabel("Data Ora Fine");
-		lblDataOraFine.setBounds(10, 61, 169, 14);
-		frame.getContentPane().add(lblDataOraFine);
+		JLabel EndDateLabel = new JLabel("Data Ora Fine");
+		EndDateLabel.setBounds(10, 61, 169, 14);
+		frame.getContentPane().add(EndDateLabel);
 		
-		JLabel lblMotivazione = new JLabel("Motivazione");
-		lblMotivazione.setBounds(10, 86, 169, 14);
-		frame.getContentPane().add(lblMotivazione);
+		JLabel reasonLabel = new JLabel("Motivazione");
+		reasonLabel.setBounds(10, 86, 169, 14);
+		frame.getContentPane().add(reasonLabel);
 		
-		JLabel lblNote = new JLabel("Note");
-		lblNote.setBounds(10, 111, 169, 14);
-		frame.getContentPane().add(lblNote);
+		JLabel noteLabel = new JLabel("Note");
+		noteLabel.setBounds(10, 111, 169, 14);
+		frame.getContentPane().add(noteLabel);
 		
-		JLabel lblDataCreazioneMessaggio = new JLabel("Data Creazione Messaggio");
-		lblDataCreazioneMessaggio.setBounds(10, 136, 169, 14);
-		frame.getContentPane().add(lblDataCreazioneMessaggio);
+		JLabel creationDateLabel = new JLabel("Data Creazione Messaggio");
+		creationDateLabel.setBounds(10, 136, 169, 14);
+		frame.getContentPane().add(creationDateLabel);
 		
-		txtUpnapolil = new JTextField();
-		txtUpnapolil.setText("UP_NAPOLIL_4");
-		txtUpnapolil.setBounds(189, 8, 169, 20);
-		frame.getContentPane().add(txtUpnapolil);
-		txtUpnapolil.setColumns(10);
+		nameText = new JTextField();
+		nameText.setText("UP_NAPOLIL_4");
+		nameText.setBounds(189, 8, 169, 20);
+		frame.getContentPane().add(nameText);
+		nameText.setColumns(10);
 		
-		txtL = new JTextField();
-		txtL.setText("16-09-2011 10:15:00 L");
-		txtL.setBounds(189, 33, 169, 20);
-		frame.getContentPane().add(txtL);
-		txtL.setColumns(10);
+		startDateText = new JTextField();
+		startDateText.setText("16-09-2011 10:15:00 L");
+		startDateText.setBounds(189, 33, 169, 20);
+		frame.getContentPane().add(startDateText);
+		startDateText.setColumns(10);
 		
-		txtL_1 = new JTextField();
-		txtL_1.setText("16-09-2011 10:28:00 L");
-		txtL_1.setBounds(189, 58, 169, 20);
-		frame.getContentPane().add(txtL_1);
-		txtL_1.setColumns(10);
+		endDateText = new JTextField();
+		endDateText.setText("16-09-2011 10:28:00 L");
+		endDateText.setBounds(189, 58, 169, 20);
+		frame.getContentPane().add(endDateText);
+		endDateText.setColumns(10);
 		
-		txtMessaggioStart = new JTextField();
-		txtMessaggioStart.setText("Messaggio START");
-		txtMessaggioStart.setBounds(189, 83, 169, 20);
-		frame.getContentPane().add(txtMessaggioStart);
-		txtMessaggioStart.setColumns(10);
+		reasonText = new JTextField();
+		reasonText.setText("Messaggio START");
+		reasonText.setBounds(189, 83, 169, 20);
+		frame.getContentPane().add(reasonText);
+		reasonText.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setText("-77.812");
-		textField_4.setBounds(189, 108, 169, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		noteText = new JTextField();
+		noteText.setText("-77.812");
+		noteText.setBounds(189, 108, 169, 20);
+		frame.getContentPane().add(noteText);
+		noteText.setColumns(10);
 		
-		txtL_2 = new JTextField();
-		txtL_2.setText("16-09-2011 09:15:00 L");
-		txtL_2.setBounds(189, 133, 169, 20);
-		frame.getContentPane().add(txtL_2);
-		txtL_2.setColumns(10);
+		creationDateText = new JTextField();
+		creationDateText.setText("16-09-2011 09:15:00 L");
+		creationDateText.setBounds(189, 133, 169, 20);
+		frame.getContentPane().add(creationDateText);
+		creationDateText.setColumns(10);
 		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(345, 238, 89, 23);
-		frame.getContentPane().add(btnCancel);
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		cancelButton.setBounds(345, 238, 89, 23);
+		frame.getContentPane().add(cancelButton);
 		
-		JButton btnOk = new JButton("OK");
-		btnOk.setBounds(246, 238, 89, 23);
-		frame.getContentPane().add(btnOk);
+		JButton okButton = new JButton("OK");
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Created dialog = new Created(nameText.getText(),
+						startDateText.getText(),
+						endDateText.getText(),
+						noteText.getText(),
+						reasonText.getText(),
+						creationDateText.getText());
+			}
+		});
+		okButton.setBounds(246, 238, 89, 23);
+		frame.getContentPane().add(okButton);
 	}
 }
